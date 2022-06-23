a0732861ff084b2fe963a045891fceadb191c11c
Konrad Geller <gellerkonrad@gmail.com>
2022-06-23 21:00:49 +0200

    Updates

---
+++ precache-manifest.79824cded15f2437259c26c3b1a8c6d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2f8a3f89471047f5edb",
+    "url": "konradge.github.io/movie-memory/static/css/main.942d5750.chunk.css"
+  },
+  {
+    "revision": "e2f8a3f89471047f5edb",
+    "url": "konradge.github.io/movie-memory/static/js/main.e2f8a3f8.chunk.js"
+  },
+  {
+    "revision": "261579908788345a9f0e",
+    "url": "konradge.github.io/movie-memory/static/css/1.e391e501.chunk.css"
+  },
+  {
+    "revision": "261579908788345a9f0e",
+    "url": "konradge.github.io/movie-memory/static/js/1.26157990.chunk.js"
+  },
+  {
+    "revision": "8c12ca8e3fd8d93928e0",
+    "url": "konradge.github.io/movie-memory/static/js/runtime~main.8c12ca8e.js"
+  },
+  {
+    "revision": "5823c32e6d211eb886e5c8021923b020",
+    "url": "konradge.github.io/movie-memory/index.html"
+  }
+];
